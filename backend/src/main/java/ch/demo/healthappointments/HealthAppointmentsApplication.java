6e50a8b49d0d7becbf97db6a0590e7521304ca2a
Laura2710 <tomasinilaura1988@gmail.com>
2026-01-21 15:55:17 +0100

    chore: initialize spring boot backend

---
+++ backend/src/main/java/ch/demo/healthappointments/HealthAppointmentsApplication.java
@@ -0,0 +1,13 @@
+package ch.demo.healthappointments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthAppointmentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthAppointmentsApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/ch/demo/healthappointments/HealthAppointmentsApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.demo.healthappointments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthAppointmentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
